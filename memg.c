da7a81ae375743316267646110768c0588f1b3e3
seungho <47456364+SeunghoCuber@users.noreply.github.com>
2023-01-13 13:47:02 -0800

    Create memg.c

---
+++ memg.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+
+    // Get the number of elements for the array
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Entered number of elements: %d\n", n);
+
+    // Dynamically allocate memory using malloc()
+    ptr = (int*)malloc(n * sizeof(int));
+
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+
+        printf("\n");
+    }
+
+    free(ptr);
+
+    //new variable
+    int* num;
+
+    num = (int*)calloc(n, sizeof(int));
+
+    if (num == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            num[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of num: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", num[i]);
+        }
+
+        printf("\n");
+    }
+
+    n += 5;
+
+    num = realloc(num, n * sizeof(int));
+
+    if (num == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            num[i] = i + 2;
+        }
+
+        // Print the elements of the array
+        printf("The new elements of num: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", num[i]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
